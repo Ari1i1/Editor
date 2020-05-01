691e8942d2ee35febb782298ce5d7b9cd1c59e6c
katefomkina <k_fomkina@mail.ru>
2020-05-02 00:25:36 +0700

    Теперь есть вызов камеры и получение фото, но оно остается в MainActivity

--- app/src/main/java/com/example/editor/MainActivity.kt
+++ app/src/main/java/com/example/editor/MainActivity.kt
@@ -1,12 +1,95 @@
 package com.example.editor
 
+import android.app.Activity
+import android.content.ContentResolver
+import android.content.ContentValues
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import android.os.Build
+import android.provider.MediaStore
+import android.widget.ImageView
+import android.widget.Toast
+import java.util.jar.Manifest
 
 class MainActivity : AppCompatActivity() {
+
+    private val PERMISSION_CODE = 1000
+    private val IMAGE_CAPTURE_CODE = 1001
+    var image_uri: Uri? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        cameraButton.setOnClickListener {
+            //if android version is Marshmallow and above
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                if (checkSelfPermission(android.Manifest.permission.CAMERA)
+                    == PackageManager.PERMISSION_DENIED || checkSelfPermission(android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                    == PackageManager.PERMISSION_DENIED
+                ) {
+                    //permission was not enable
+                    val permission = arrayOf(
+                        android.Manifest.permission.CAMERA,
+                        android.Manifest.permission.WRITE_EXTERNAL_STORAGE
+                    )
+                    //show pop-up to request permission
+                    requestPermissions(permission, PERMISSION_CODE)
+                } else {
+                    //permission was granted
+                    openCamera()
+                }
+            }
+            else {
+            //android version < Marshmallow
+            openCamera()
+            }
+        }
+    }
+
+    private fun openCamera(){
+        val values = ContentValues()
+        values.put(MediaStore.Images.Media.TITLE, "New picture")
+        values.put(MediaStore.Images.Media.DESCRIPTION, "From the Camera")
+        image_uri = contentResolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values)
+        //camera
+        val cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, image_uri)
+        startActivityForResult(cameraIntent, IMAGE_CAPTURE_CODE)
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        //called when user press Allow or Deny
+        when (requestCode) {
+            PERMISSION_CODE -> {
+                if (grantResults.size > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    //permission from pop-up was granted
+                    openCamera()
+                }
+                else {
+                    //permission from pop-up was denied
+                    Toast.makeText(this, "Permission denied", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        //called when image was captured from camera intent
+        if (resultCode == Activity.RESULT_OK) {
+            //set image captured to image view
+            viewer.setImageURI(image_uri)
+        }
     }
 }
+
