90049c19bd78663c549a84c42b32ab1d880e1d01
Ариадна <ariadna@MacBook-Pro-Ariadna.local>
2020-05-05 19:28:20 +0700

    добавлен запрос фото из галереи

--- app/src/main/java/com/example/editor/MainActivity.kt
+++ app/src/main/java/com/example/editor/MainActivity.kt
@@ -16,6 +16,7 @@ class MainActivity : AppCompatActivity() {
 
     private val PERMISSION_CODE = 1000
     private val IMAGE_CAPTURE_CODE = 1001
+    private val IMAGE_PICK_CODE = 1001
     var image_uri: Uri? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -46,6 +47,27 @@ class MainActivity : AppCompatActivity() {
                 openCamera()
             }
         }
+
+        galleryButton.setOnClickListener {
+            //check runtime permission
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+                if (checkSelfPermission(android.Manifest.permission.READ_EXTERNAL_STORAGE) ==
+                    PackageManager.PERMISSION_DENIED){
+                    //permission denied
+                    val permissions = arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE);
+                    //show popup to request runtime permission
+                    requestPermissions(permissions, PERMISSION_CODE);
+                }
+                else{
+                    //permission already granted
+                    pickImageFromGallery();
+                }
+            }
+            else{
+                //system OS is < Marshmallow
+                pickImageFromGallery();
+            }
+        }
     }
 
     private fun openCamera(){
@@ -57,6 +79,13 @@ class MainActivity : AppCompatActivity() {
         val cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
         cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, image_uri)
         startActivityForResult(cameraIntent, IMAGE_CAPTURE_CODE)
+    }
+
+    private fun pickImageFromGallery() {
+        //Intent to pick image
+        val intent = Intent(Intent.ACTION_PICK)
+        intent.type = "image/*"
+        startActivityForResult(intent, IMAGE_PICK_CODE)
     }
 
     override fun onRequestPermissionsResult(
