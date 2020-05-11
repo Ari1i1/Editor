fc8424f6dd4598c7064e291338b40c7653edeadc
ariadna <afanask1nanyc@gmail.com>
2020-05-11 23:00:43 +0700

    ff

--- app/src/main/java/com/example/editor/EditingActivity.kt
+++ app/src/main/java/com/example/editor/EditingActivity.kt
@@ -10,7 +10,10 @@ import kotlinx.android.synthetic.main.activity_editing.*
 import kotlin.math.cos
 import kotlin.math.sin
 
+
 class EditingActivity : AppCompatActivity() {
+
+    private val editableImage: Bitmap = (imageView.drawable as BitmapDrawable).bitmap
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,22 +25,21 @@ class EditingActivity : AppCompatActivity() {
         imageView.setImageURI(uri)
 
         rotateButton.setOnClickListener{
-            val editableImage: Bitmap = (imageView.drawable as BitmapDrawable).bitmap
-
-            val width: Int = editableImage.width
-            val height: Int = editableImage.height
+            val width: Int = this.editableImage.getWidth()
+            val height: Int = this.editableImage.getHeight()
             val angle = Math.toRadians(90.0)
             val sin = sin(angle)
             val cos = cos(angle)
             val rotationPoint = 0.5 * (width - 1) // point to rotate about
             val imageCenter = 0.5 * (height - 1) // center of image
-            val editedImage = Bitmap.createBitmap(height, width, editableImage.config)
+            val editedImage = createBitmap(width, height, Bitmap.Config.ALPHA_8)
+//        val pixel = IntArray(3)
 
-            //rotation
-            for (x in 0 until height) {
-                for (y in 0 until width) {
-                    val a = x - imageCenter
-                    val b = y - rotationPoint
+            // rotation
+            for (x in 0 until width) {
+                for (y in 0 until height) {
+                    val a = x - rotationPoint
+                    val b = y - imageCenter
                     val xx = (+a * cos - b * sin + rotationPoint).toInt()
                     val yy = (+a * sin + b * cos + imageCenter).toInt()
                     if (xx in 0 until width && yy >= 0 && yy < height) {
@@ -48,8 +50,9 @@ class EditingActivity : AppCompatActivity() {
             imageView.setImageBitmap(editedImage)
         }
     }
-}
 
+
+}
 
 
 
